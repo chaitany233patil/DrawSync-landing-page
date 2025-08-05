60e48edb2e64b7aae16ee0208de743825483eef7
Chaitany233patil <patilchaitany529@gmail.com>
2025-08-05 19:25:25 +0530

    added new video

--- components/Video.tsx
+++ components/Video.tsx
@@ -2,11 +2,12 @@ export function Video() {
   return (
     <div>
       <div style={{ position: "relative", paddingBottom: "56.25%", height: 0 }}>
-        <iframe
-          src="https://www.loom.com/embed/7ec16b392ecc42b092b12958036840f8?sid=693f20a2-453b-4df6-b5b4-9d74812a9d3f"
-          frameBorder="0"
-          allowFullScreen
-          allow="autoplay; fullscreen"
+        <video
+          src="/video.mp4"
+          autoPlay
+          loop
+          muted
+          playsInline
           style={{
             position: "absolute",
             top: 0,
@@ -14,7 +15,7 @@ export function Video() {
             width: "100%",
             height: "100%",
           }}
-        ></iframe>
+        />
       </div>
     </div>
   );
